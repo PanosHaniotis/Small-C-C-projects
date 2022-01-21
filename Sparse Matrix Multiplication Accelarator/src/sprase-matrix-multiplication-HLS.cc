bce39c94200eca050872526cd7178352353911f0
Panagiotis Chaniotis <92161243+pChaniotis@users.noreply.github.com>
2022-01-21 03:55:57 +0200

    Add files via upload

--- Sparse Matrix Multiplication Accelarator/src/sprase-matrix-multiplication-HLS.cc
+++ Sparse Matrix Multiplication Accelarator/src/sprase-matrix-multiplication-HLS.cc
@@ -5,7 +5,7 @@ typedef std::vector<int> vect;
 typedef std::vector<std::vector<int> > matrix;
 
 #define ROW 0
-#define COLLUM 1
+#define COLUMN 1
 #define VALUE 2
 
 
@@ -17,83 +17,23 @@ class sparse{
 	int NNZ;
 	matrix data;
 	
-	private:
-		
-	
-	
-	
 	public://-------methods---------
 	
-	
-	//keno object gia epistrofh sthn pra3h tou pollaplasiasmou
-	sparse(int row, int col){
-		NNZ = 0;
-		r = row;
-		c = col;
-	}
-	
+
 	
 	sparse(const matrix& matrix){
  	
 		NNZ = 0;
-		
 	 	r = matrix.size();
 		c = matrix[0].size();
 		
+		convertToCSR(matrix);
+		
 		
  		
- 		int i,j;
- 		
- 		for (i = 0; i < r; i++){
-	        for (j = 0; j < c; j++){
-	            if (matrix[i][j] > 0){
-
-					insertToCSR(i, j, matrix[i][j]);
-				}
-			}
-		}					
+ 						
 	}
 	
-	
-	sparse operator*(const sparse& b){
-		
-		
-		
-		matrix C;
-		C.assign (4,{1,1,1,1});
-		//matrix C[ data[NNZ][ROW] ] [ b.data[NNZ][COLLUM] ];
-		
-//		for (int i=0; i<NNZ-1; i++) {
-//		  for (int j=0; j<b.NNZ-1; j++) {
-//		   
-//		    for (int m=data[i][ROW]; m < data[i+1][ROW]; m++) {
-//		      int k=data[m][ROW];
-//		      for (int p=b.data[j][COLLUM]; p < b.data[j+1][COLLUM]; p++) {
-//		        if (b.data[p][ROW] == k){
-//				
-//		          C[m][p]  = C[m][p] +  data[m][VALUE]*b.data[p][VALUE];
-//		          
-//		      	}
-//		      }
-//		    }
-//		  }
-//		}
-
-		return sparse(C);
-	}
-
-	void convertCSRToCSC(){ 
-	    int i, j; 
-	    for (i = 0; i < NNZ-1; i++)     
-	      
-	    // Last i elements are already in place 
-	    for (j = 0; j < NNZ-i-1; j++) 
-	        if (data[j][COLLUM] > data[j+1][COLLUM]) 
-	            data[j].swap(data[j+1]);
-
-    }
-
-		
 
 	
 	void insertToCSR(int r, int c, int val){
@@ -101,6 +41,7 @@ class sparse{
 		data.push_back({r,c,val});
 	 	NNZ++;
 	}
+	
 	void printData(){
 	    std::cout << "\nDimension of Sparse Matrix: " << NNZ << " x " << 3;
 	    std::cout << "\nSparse Matrix: \nRow  Column  Value\n";
@@ -108,38 +49,162 @@ class sparse{
 	    
 		for (int i = 0; i < NNZ; i++) {
  
-        	std::cout << data[i][ROW] << " 	" << data[i][COLLUM] << "	" << data[i][VALUE] << "\n";
+        	std::cout << data[i][ROW] << " 	" << data[i][COLUMN] << "	" << data[i][VALUE] << "\n";
     	}
 	}
+	
+	void convertToCSR(const matrix& matrix){
+		
+		for (int i = 0; i < r; i++){
+	        for (int j = 0; j < c; j++){
+	            if (matrix[i][j] > 0){
+
+					insertToCSR(i, j, matrix[i][j]);
+				}
+			}
+		}	
+		
+	}
+	void convertCSRToCSC(){ 
+	    int i, j; 
+	    for (i = 0; i < NNZ-1; i++)     
+	      
+	    // Last i elements are already in place 
+	    for (j = 0; j < NNZ-i-1; j++) 
+	        if (data[j][COLUMN] > data[j+1][COLUMN]) 
+	            data[j].swap(data[j+1]);
+
+    }
+    
+    
+    
+    
+    sparse operator*(sparse b){
+		
+		
+		if(this->r != b.r){
+		
+			std::cout<<"\nIllegal matrix boundaries\n";
+			matrix error = {{0,0},{0,0}};
+			return sparse(error);
+		}
+		
+		//initalize a a.row x b.column matrix
+		vect vec;
+		vec.assign(b.c,0);
+		
+		matrix C;
+		C.assign (this->r,vec);
+		
+		//convert b to CSC
+		//b.convertCSRToCSC();
+		
+		
+		
+		
+		for(int i = 0;i < this->NNZ;i++){
+			
+			for(int j = 0;j < b.NNZ;j++){
+				
+				if(b.data[j][ROW] == this->data[i][COLUMN] ){
+					
+					C[this->data[i][ROW]][b.data[j][COLUMN]] += this->data[i][VALUE] * b.data[j][VALUE];
+				}
+				
+			}			
+			
+		}
+		
+		return sparse(C);
+	}
+	
+	
 	
 };
 
 
+matrix mul(matrix& a, matrix& b){
+	
+	int ra = a.size();
+	int ca = a[0].size();
+
+	int rb = b.size();
+	int cb = b[0].size();	
+	
+	if(ra != cb){
+		
+		std::cout<<"\nIllegal matrix boundaries\n";
+		return {{0,0},{0,0}};
+	}
+	
+	vect vec;
+	vec.assign(cb,0);
+		
+	matrix C;
+	C.assign (ra,vec);
+	
+	std::cout<<"Matrix "<<ra<<"x"<<cb<<":"<<std::endl;
+	
+	for (int i = 0; i < ra; i++) {
+        for (int j = 0; j < cb; j++) {
+
+            for (int k = 0; k < rb; k++) {
+            	
+                C[i][j] += a[i][k] * b[k][j];
+            }
+
+            std::cout << C[i][j] << "\t";
+        }
+
+        std::cout << std::endl;
+    }
+	
+	return C;
+
+	
+}
 
 
 
 int main(){
 	
+	
+
+	
 	matrix a =
 	    {
-	        {1, 0, 0, 0},
-	        {0, 5, 0, 1},
-	        {7, 0, 0, 0}
+	        {1, 0, 0},
+	        {0, 0, 5},
+	        {7, 0, 0}
 	    };
+	    
+	    
+	matrix b =
+	    {
+	        {0, 0, 2},
+	        {0, 1, 0},
+	        {8, 6, 0}
+
+	    };
+	
+	
+	
+	    
+	    
 	
 	
 	sparse obj_a(a);
 	obj_a.printData();
 			
-	sparse obj_b(4,3);
-	obj_b.insertToCSR(1,1,100);
+	sparse obj_b(b);
+	obj_b.convertCSRToCSC();
 	obj_b.printData();
 	
-	obj_a.convertCSRToCSC();
-	obj_a.printData();
+	
+	
+	mul(a,b);
 	
 	sparse obj_c = obj_a*obj_b;
-	
 	obj_c.printData();
 	
 
